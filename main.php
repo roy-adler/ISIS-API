d375464c864fb144b822abfce4962daef6322c04
Roy Adler <royadler@outlook.com>
2020-11-03 16:22:02 +0100

    stage test 1

--- main.php
+++ main.php
@@ -4,3 +4,9 @@ $login_site = "https://shibboleth.tubit.tu-berlin.de/idp/profile/SAML2/Redirect/
 $webscraper = new Webscraper($login_site);
 $curled_text = $webscraper->curl();
 echo $curled_text;
+afwf
+gin_site = "https://shibboleth.tubit.adjhasjkdhtu-berlin.de/idp/profile/SAML2/Redirect/SSO?execution=e1s2";
+$webscraper = new Webscraper($login_site);
+$curled_text = $webscraper->curl();
+echo $curled_text;
+gin_site = "https://shibboleth.tubit.adjhasjkdhtu-berlin.de/idp/profile/SAML2/Redirect/SSO?execution=e1s2";
